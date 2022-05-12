39643b3335534a164d01dc1217f9f25d8f338ddd
fengwu <781183149@qq.com>
2022-05-12 16:49:45 +0800

    feat: 增加netty

---
+++ netty/src/main/java/com/github/hahahehejun/netty/server/HelloWordServer.java
@@ -0,0 +1,13 @@
+package com.github.hahahehejun.netty.server;
+
+/**
+ * @author wu
+ * @date 2022/5/12 16:49
+ */
+public class HelloWordServer {
+
+    public static void main(String[] args) {
+
+    }
+
+}
